bf1c2f2f003964f744fffa4dd48349b30132cdfc
Jacob Kinchen <144300546+Jacon-ator@users.noreply.github.com>
2025-03-09 21:55:59 -0500

    Added Map Page

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,8 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
   static final List<Widget> _pages = <Widget>[
-    HuntPage(),
     MapPage(),
+    HuntPage(),
     HelpPage(),
   ];
 
@@ -93,11 +93,13 @@ class MapPage extends StatelessWidget {
 }
 
 class Question {
+  final String title; // New custom title field
   final String funFact;
   final String questionText;
   final String answer;
 
   Question({
+    required this.title,
     required this.funFact,
     required this.questionText,
     required this.answer,
@@ -113,19 +115,30 @@ class HuntPage extends StatefulWidget {
 
 class _HuntPageState extends State<HuntPage> {
   int currentQuestionIndex = 0;
+  bool isStarted = false; // Tracks whether the hunt has started
   final TextEditingController _answerController = TextEditingController();
   String feedback = "";
 
   final List<Question> questions = [
     Question(
-      funFact: "Test Question 1",
-      questionText: "Write 'blue' ?",
-      answer: "blue",
+      title: "Capstone Gallery",
+      funFact:
+          "The Capstone Gallery is one of the favorite study spots for most students.",
+      questionText: "How many steps does it take to walk up the large stairs?",
+      answer: "35",
     ),
     Question(
-      funFact: "Test Question 2",
-      questionText: "Write 'yellow'",
-      answer: "yellow",
+      title: "Cambre Atrium",
+      funFact: "The Cambre Atrium is another common study spot for students!",
+      questionText: "How many chairs are at each table?",
+      answer: "8",
+    ),
+    Question(
+      title: "Panera Bread",
+      funFact:
+          "The Panera Bread is the one stop shop for all of your food needs while studying hard!",
+      questionText: "What is the price for a bowl of tomato soup?",
+      answer: "4.99",
     ),
   ];
 
@@ -135,13 +148,14 @@ class _HuntPageState extends State<HuntPage> {
 
     if (userAnswer == correctAnswer) {
       setState(() {
-        feedback = "Correct!";
-
         if (currentQuestionIndex < questions.length - 1) {
+          feedback = "Correct!";
           currentQuestionIndex++;
           _answerController.clear();
         } else {
           feedback = "Congratulations! You completed the scavenger hunt.";
+          // Increment to mark completion
+          currentQuestionIndex++;
         }
       });
     } else {
@@ -155,54 +169,99 @@ class _HuntPageState extends State<HuntPage> {
   Widget build(BuildContext context) {
     bool isCompleted = currentQuestionIndex >= questions.length;
 
+    // Show welcome screen if hunt hasn't started
+    if (!isStarted) {
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Start your tour at the Capstone Gallery!",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isStarted = true;
+                  });
+                },
+                child: Text("Start"),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    // Show completion message if all questions are answered
+    if (isCompleted) {
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                  "You've completed the scavenger hunt! You now are an expert on PFT! Have a nice day!",
+                  style: TextStyle(fontSize: 24)),
+            ],
+          ),
+        ),
+      );
+    }
+
+    // Display the current question using the custom title
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Center(
-        child: isCompleted
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("You've completed the scavenger hunt!",
-                      style: TextStyle(fontSize: 24)),
-                ],
-              )
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Question ${currentQuestionIndex + 1}",
-                      style: TextStyle(fontSize: 24)),
-                  SizedBox(height: 20),
-                  Text(questions[currentQuestionIndex].funFact,
-                      style: TextStyle(fontSize: 18)),
-                  SizedBox(height: 20),
-                  Text(questions[currentQuestionIndex].questionText,
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 20),
-                  TextField(
-                    controller: _answerController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Your Answer',
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: checkAnswer,
-                    child: Text('Submit'),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    feedback,
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: feedback == "Correct!" ||
-                                feedback.contains("Congratulations")
-                            ? Colors.green
-                            : Colors.red),
-                  ),
-                ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Custom header for each question
+            Text(
+              questions[currentQuestionIndex].title,
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Text(
+              questions[currentQuestionIndex].funFact,
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            Text(
+              questions[currentQuestionIndex].questionText,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _answerController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Your Answer',
               ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: checkAnswer,
+              child: Text('Submit'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              feedback,
+              style: TextStyle(
+                fontSize: 18,
+                color: feedback == "Correct!" ||
+                        feedback.contains("Congratulations")
+                    ? Colors.green
+                    : Colors.red,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -231,7 +290,7 @@ class HelpPage extends StatelessWidget {
           SizedBox(height: 4),
           Text.rich(
             TextSpan(
-              text: 'Contact me at:',
+              text: 'Contact me a:',
               style: TextStyle(fontSize: 18),
               children: <TextSpan>[
                 TextSpan(
